b5cff1d5481cc1d91ea1afe3c98037f5a2479626
Higgins Weng <wenld1601@gmail.com>
2019-10-24 02:03:26 -0700

    ParkinSense 1.3 (with login)

--- ParkinSense/ParkinSense/ViewController/HomeViewController.swift
+++ ParkinSense/ParkinSense/ViewController/HomeViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+
+    }
 
     /*
     // MARK: - Navigation
