7c75db1fd5677ef1b8cdbebabac08f7f4e06db37
Higgins Weng <wenld1601@gmail.com>
2019-10-24 17:07:05 -0700

    ParkinSense 1.4 (with login)

---
+++ ParkinSense/ParkinSense/Helper/WeeklyCalendar.swift
@@ -0,0 +1,20 @@
+//
+//  WeeklyCalendar.swift
+//  ParkinSense
+//
+//  Created by weng Higgins on 2019-10-24.
+//  Copyright © 2019 PDD Inc. All rights reserved.
+//
+
+import Foundation
+
+let date = Date()
+let calendar = Calendar.current
+
+let day = calendar.component(.day, from: date)
+let weekday = calendar.component(.weekday, from: date)
+
+
+
+
+
